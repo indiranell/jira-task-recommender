ec4998d4a5a49dada2944b949e082b1b364a9d2c
Nellutla, Indira <indira.nellutla@sciencelogic.com>
2020-07-02 13:31:22 +0530

    mapped the issue summary with time spent

--- task_recommender.py
+++ task_recommender.py
@@ -12,12 +12,12 @@ class LocalJira(JIRA):
 
     def get_projects(self):
         "Get the projects"
-        
+
         return self.projects()
 
-    def get_issues(project_id):
+    def get_issues(self,project_id):
         "Get the Issues"
-        issues = jira.search_issues(project_id)
+        issues = self.search_issues("project='%s'"%project_id)
 
         return issues
 
@@ -33,10 +33,10 @@ class LocalJira(JIRA):
                         time_spent['doing'] = history.created.split('+')[0]
                     elif(item.toString == 'Done'):
                         time_spent['done'] = history.created.split('+')[0]
+        if(time_spent.get('done') and time_spent.get('doing')):
+            time_difference = datetime.datetime.fromisoformat(time_spent['done']) - datetime.datetime.fromisoformat(time_spent['doing'])
 
-        time_difference = datetime.datetime.fromisoformat(time_spent['done']) - datetime.datetime.fromisoformat(time_spent['doing'])
-
-        return time_difference
+            return time_difference.days * 8
 
     def get_projects_using_keyword(self,keyword):
         "Get the projects that has the given keyword"
@@ -45,12 +45,18 @@ class LocalJira(JIRA):
         for project in projects:
             if keyword.lower() in project.name.lower():
                 req_projects.append(project)
-        
+
         return req_projects
 
 if __name__ == '__main__':
     obj = LocalJira(server,username,apitoken)
-    print(obj.get_projects_using_keyword('onboarding'))
+    projects = obj.get_projects_using_keyword('onboading')
+    issues = {}
+    for project in projects:
+        issues['project'] = obj.get_issues(project)
+        for issue in issues['project']:
+            time_diff = obj.get_time_spent(issue.id)
+            print(issue.raw['fields']['summary'],time_diff)
 
 
 
